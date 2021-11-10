3715a3a6bb71a3baefd3bc3127dd6e39500de76a
Chuong Vo <vohoang2005@gmail.com>
2021-11-09 16:42:59 -0800

    start and end game functions

--- assets/js/game.js
+++ assets/js/game.js
@@ -94,3 +94,54 @@ for (var i = 0; i < enemyNames.length; i++) {
     break;
   }
 }
+
+
+
+// function to start a new game
+var startGame = function() {
+    for (var i = 0; i < enemyNames.length; i++) {
+
+    }
+    endGame();
+    playerHealth = 100;
+    playerHealth = 10;
+    playerMoney = 10;
+
+    for (var i = 0; i < enemyNames.length; i++) {
+      if (playerHealth > 0) {
+        window.alert("Welcome to Robot Gladiators! Round " + (i + 1));
+  
+        var pickedEnemyName = enemyNames[i];
+  
+        enemyHealth = 50;
+  
+        fight(pickedEnemyName);
+      }
+      
+      else {
+        window.alert("You have lost your robot in battle! Game Over!");
+        break;
+      }
+    }
+    
+  };
+
+  var endGame = function() {
+      if (playerHealth > 0) {
+          window.alert("Great job, you 've survived the game! You now have a score of " + playerMoney + ".");
+      }
+      else {
+          window.alert("You've lost your robot in battle.");
+      }
+  }
+  var playAgainConfirm = window.confirm("Would you like to play again?");
+
+  if (playAgainConfirm) {
+      startGame();
+  }
+  else {
+      window.alert("Thank you for playing Robot Gladiators! Come back soon!")
+  }
+
+  // start the game when the page loads
+startGame();
